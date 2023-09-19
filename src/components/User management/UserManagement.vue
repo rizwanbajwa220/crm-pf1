8d2f220bf3f971f12f47c7a04e3b4847db25bed0
kashif <kashif_saleem_23@hotmail.com>
2023-09-19 15:31:14 +0500

    Feat: Added table for user management

---
+++ src/components/User management/UserManagement.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-data-table
+    v-model:items-per-page="itemsPerPage"
+    :headers="headers"
+    :items="User"
+    item-value="name"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  name: "UserManagement",
+  data() {
+    return {
+      itemsPerPage: 5,
+      headers: [
+        {
+          title: "First Name",
+          align: "start",
+          sortable: false,
+          key: "name",
+        },
+        { title: "Last Name", align: "center", key: "lastName" },
+        { title: "Email", align: "center", key: "Email" },
+        { title: "Role", align: "center", key: "role" },
+        { title: "Actions", align: "center", key: "Actions" },
+      ],
+      User: [
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -3,19 +3,25 @@
     <v-navigation-drawer v-model="drawer">
       <v-list :items="items">
         <template v-slot:prepend="{ item }">
-          <v-list-item-icon>
-            <v-icon class="icon-style">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+          <v-list-item @click="selectItem(item)">
+            <v-list-item-icon>
+              <v-icon class="icon-style">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
         </template>
       </v-list>
     </v-navigation-drawer>
 
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-app-bar-title>Application</v-app-bar-title>
+      <v-app-bar-title>{{
+        selectedItem ? selectedItem.title : "User Management"
+      }}</v-app-bar-title>
     </v-app-bar>
 
-    <v-main> Hello </v-main>
+    <v-main>
+      <UserManagement :FirstName="UserData" />
+    </v-main>
   </v-app>
 </template>
 
@@ -26,9 +32,11 @@ const drawer = ref(null);
 </script>
 
 <script>
+import UserManagement from "@/components/User management/UserManagement";
 export default {
   data: () => ({
     drawer: null,
+    selectedItem: null,
     items: [
       {
         title: "User Management",
@@ -48,11 +56,17 @@ export default {
       },
     ],
   }),
+  methods: {
+    selectItem(item) {
+      this.selectedItem = item;
+      this.drawer = false; // Close the drawer when an item is selected (optional)
+    },
+  },
 };
 </script>
 
 <style scoped>
 .icon-style {
-  margin-right: 10px;
+  margin-right: 5px;
 }
 </style>
