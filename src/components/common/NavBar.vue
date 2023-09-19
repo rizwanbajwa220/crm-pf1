b769440956bd0411423ee8edcc76bcc8c5dbec3b
kashif <kashif_saleem_23@hotmail.com>
2023-09-19 13:07:38 +0500

    Refac Updated Nav bar to include list items

--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -1,29 +1,58 @@
 <template>
-    <v-app id="inspire">
-      <v-navigation-drawer v-model="drawer">
-        <!--  -->
-      </v-navigation-drawer>
-  
-      <v-app-bar>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-  
-        <v-app-bar-title>Application</v-app-bar-title>
-      </v-app-bar>
-  
-      <v-main>
-        <!--  -->
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script setup>
-    import { ref } from 'vue'
-  
-    const drawer = ref(null)
-  </script>
-  
-  <script>
-    export default {
-      data: () => ({ drawer: null }),
-    }
-  </script>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-list :items="items">
+        <template v-slot:prepend="{ item }">
+          <v-list-item-icon>
+            <v-icon class="icon-style">{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main> Hello </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const drawer = ref(null);
+</script>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      {
+        title: "User Management",
+        // add icon
+        icon: "mdi-account",
+        value: 1,
+      },
+      {
+        title: "Task Management",
+        icon: "mdi-account",
+        value: 2,
+      },
+      {
+        title: "Role Management",
+        icon: "mdi-account",
+        value: 3,
+      },
+    ],
+  }),
+};
+</script>
+
+<style scoped>
+.icon-style {
+  margin-right: 10px;
+}
+</style>
