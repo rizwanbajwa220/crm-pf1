f3737c0d4f14579deb918c9a9818918af777981e
Muhammad Abdullah <abdullahbhatti3399@gmail.com>
2023-09-19 21:27:30 -0700

    added roles and permissions

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import UpdateModal from '@/components/updateRoles/UpdateModal.vue'
+import RolesTable from '@/components/RoleManagement/RolesTable.vue'
 
 const routes = [
   {
@@ -11,6 +13,16 @@ const routes = [
         name: 'Home',
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: '/update-roles',
+        name: 'update-roles',
+        component: UpdateModal,
+      },
+      {
+        path: '/add-roles',
+        name: 'add-roles',
+        component: RolesTable,
+      }
     ],
   },
 ]
