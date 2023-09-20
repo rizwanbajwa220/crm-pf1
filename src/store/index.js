3cf4fdb7e643d518866731520db33499e69dd4f7
kashif <kashif_saleem_23@hotmail.com>
2023-09-20 07:00:29 +0500

    Feat: Setup up store with the users data state

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,77 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    userData: {
+      itemsPerPage: 5,
+      headers: [
+        {
+          title: "First Name",
+          align: "start",
+          sortable: false,
+          key: "name",
+        },
+        { title: "Last Name", align: "center", key: "lastName" },
+        { title: "Email", align: "center", key: "Email" },
+        { title: "Role", align: "center", key: "role" },
+        { title: "Actions", align: "center", key: "Actions" },
+      ],
+      users: [
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+        {
+          name: "Kashif",
+          lastName: "Saleem",
+          Email: "kashif@example.com",
+          role: "Admin",
+          Actions: [
+            { icon: "mdi-pencil", color: "primary" },
+            { icon: "mdi-delete", color: "error" },
+          ],
+        },
+      ],
+    },
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
