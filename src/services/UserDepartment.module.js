918bec28662bb75afec25be71b5431bd41e7e00e
kashif <kashif_saleem_23@hotmail.com>
2023-09-21 17:40:42 +0500

    Refac: Api fetched and data dispalyed for all users

--- src/services/UserDepartment.module.js
+++ src/services/UserDepartment.module.js
@@ -4,7 +4,7 @@ const BASE_URL = "http://10.0.10.34:3500/api";
 
 const state = {
   userData: {
-    itemsPerPage: 3,
+    itemsPerPage: 5,
     headers: [
       {
         title: "Name",
@@ -12,16 +12,18 @@ const state = {
         sortable: false,
         key: "name",
       },
-      { title: "Email", align: "center", key: "Email" },
-      { title: "Created_At", align: "center", key: "role" },
-      { title: "Actions", align: "center", key: "Actions" },
+      { title: "Email", align: "center", key: "email" },
+      { title: "Created_At", align: "center", key: "created_at" },
+      { title: "Actions", align: "center", key: "actions" },
     ],
-    users: null,
+    users: [],
   },
 };
 
 const getters = {
   getUsers: (state) => state.userData.users,
+  getHeaders: (state) => state.userData.headers,
+  getItemsPerPage: (state) => state.userData.itemsPerPage,
 };
 
 const mutations = {
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,6 @@ import userManagement from "@/services/UserDepartment.module.js";
 const BASE_URL = "http://10.0.10.41:3500";
 export default createStore({
   modules: {
-    userManagement,
+    u: userManagement,
   },
 });
