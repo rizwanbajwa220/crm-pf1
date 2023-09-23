a6ae0fa0c4ff6405129b753a7c11f401309c769a
kashif <kashif_saleem_23@hotmail.com>
2023-09-23 19:24:03 +0500

    Refac: Hardcoded the permissions in the permission array in the store as the API is not working, Note: to be removed when the API is live

--- src/store/auth/login.module.js
+++ src/store/auth/login.module.js
@@ -3,12 +3,33 @@ import router from "../../router/index.js";
 
 const state = {
   loginCredentials: null,
-  permissions: [],
+  permissions: [
+    "can-access-all-users",
+    "can-create-task",
+    "can-view-task",
+    "can-delete-task",
+    "can-update-task",
+    "can-reassign-task",
+    "can-view-team",
+    "can-create-team",
+    "can-update-team",
+    "can-view-specific-team",
+    "can-delete-team",
+    "can-view-members",
+    "can-create-members",
+    "can-view-specific-member",
+    "can-update-member",
+    "can-delete-member",
+    "can-add-department",
+    "can-update-department",
+    "can-delete-department",
+    "can-view-department",
+  ],
 };
 
 const mutations = {
   SET_LOGIN_CREDENTIALS(state, data) {
-    localStorage.setItem('token', data.token);
+    localStorage.setItem("token", data.token);
     state.loginCredentials = data;
 
     // Extract and store permissions
@@ -20,8 +41,8 @@ const actions = {
   async loginUser({ commit }, { email, password }) {
     try {
       const response = await ApiServices.login(email, password);
-      commit('SET_LOGIN_CREDENTIALS', response);
-      router.push({ path: '/admin-dashboard' });
+      commit("SET_LOGIN_CREDENTIALS", response);
+      router.push({ path: "/admin-dashboard" });
 
       // Log state.permissions in the next tick of the event loop
       setTimeout(() => {
